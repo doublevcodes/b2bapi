e07841b9fe8583c2b3d580e53facb6470f0187aa
Rick Astley <54081925+doublevcodes@users.noreply.github.com>
2021-02-26 18:27:48 +0000

    Added endpoints file

---
+++ meme.py
@@ -0,0 +1,9 @@
+class Meme:
+    def __init__(self, title: str, url: str, link: str, subreddit: str) -> None:
+        self.title = title
+        self.url = url
+        self.link = link
+        self.subreddit = subreddit
+
+    def __str__(self):
+        return self.title
