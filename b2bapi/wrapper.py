db8c65fd31d6a390223666b8d12b0bc9bc527d02
repl.it user <replituser@example.com>
2021-02-27 01:46:35 +0000

    Testinf

--- b2bapi/wrapper.py
+++ b2bapi/wrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from madlibs import Madlib
 
 class Meme:
     def __init__(self, title: str, url: str, link: str, subreddit: str) -> None:
@@ -11,31 +12,6 @@ class Meme:
     def __str__(self):
         return self.title
 
-class Madlib:
-
-    def __init__(self, title: str, text: str, number_of_questions: int, questions: list) -> None:
-        self.title = title
-        self.text  = text
-        self.number_of_questions = number_of_questions
-        self.questions = questions
-
-    def __iter__(self) -> iter:
-        return iter(self.questions)
-
-    def __str__(self):
-        return self.text
-
-    def get_responses(self) -> tuple:
-        ret = []
-        for question in self:
-            print(f'Please enter a/an {question}')
-            ret.append(input())
-        self.responses = ret
-        return tuple(ret)
-    
-    def substitute(self) -> str:
-        self.text = self.text.format(*self.responses)
-        return self.text
 
 class Word:
     
