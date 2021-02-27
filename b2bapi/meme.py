91a54496cf264c0907e3def1c755a63381ad75ac
Rick Astley <vivaan.verma@gmail.com>
2021-02-27 02:25:57 +0000

    Revert

--- b2bapi/meme.py
+++
@@ -1,6 +0,0 @@
-class Meme:
-    def __init__(self, title: str, url: str, link: str, subreddit: str) -> None:
-        self.title = title
-        self.url = url
-        self.link = link
-        self.subreddit = subreddit
--- b2bapi/word.py
+++
@@ -1,7 +0,0 @@
-class Word:
-    
-    def __init__(self, word):
-        self.word = word
-
-    def __str__(self):
-        return self.word
