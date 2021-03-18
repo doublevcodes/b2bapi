3fd5798c9d64a642e9b8d8844d92f27578fd9ef4
Vivaan Verma <vivaan.verma@gmail.com>
2021-03-18 19:36:03 +0000

    Made error a package

---
+++ b2bapi/errors/__init__.py
@@ -0,0 +1,6 @@
+
+from .invalid_token import InvalidTokenError
+from .rate_limit import RateLimitError
+from .response_check import response_check, token_check
+from .unauthorised import UnauthorisedError
+from .unknown import UnknownError
